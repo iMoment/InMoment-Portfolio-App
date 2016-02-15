9d99311d613df4c48067aec9299c9b6108de8270
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-15 13:00:03 +0900

    Add launch screen image, MaterialView class, basic layout

---
+++ In Moment/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  In Moment
+//
+//  Created by Stanley Pan on 2/15/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
