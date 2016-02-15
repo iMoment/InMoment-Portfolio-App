18f1aff8a3befe12d52c4e5c8631abc4e6109af6
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-15 19:31:24 +0900

    Add @IBAction facebookButtonPressed

--- In Moment/ViewController.swift
+++ In Moment/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func facebookButtonPressed(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with Facebook. \(accessToken)")
+            }
+        }
     }
 }
 
