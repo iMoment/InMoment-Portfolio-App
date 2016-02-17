6902109676dec1cbd5018a8ce2254d7bd1377eae
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-18 03:58:10 +0900

    Create FeedVC boilerplate

---
+++ In Moment/FeedVC.swift
@@ -0,0 +1,35 @@
+//
+//  FeedVC.swift
+//  In Moment
+//
+//  Created by Stanley Pan on 2/18/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
