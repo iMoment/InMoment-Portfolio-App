8875316f92f8e754a206320e396e8a50e5068431
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-23 12:22:35 +0900

    Create post class for model, parse Firebase

--- In Moment/FeedVC.swift
+++ In Moment/FeedVC.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +20,22 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
+        //  Talk to Firebase
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
+            
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDictionary = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDictionary)
+                        self.posts.append(post)
+                    }
+                }
+            }
             self.tableView.reloadData()
         })
     }
@@ -29,12 +45,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
     
     
 }
+
+
+
+
+
+
+
+
---
+++ In Moment/Post.swift
@@ -0,0 +1,66 @@
+//
+//  Post.swift
+//  In Moment
+//
+//  Created by Stanley Pan on 2/22/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String!
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    init(description: String, imageUrl: String?, username: String) {
+        self._postDescription = description
+        self._imageUrl = imageUrl
+        self._username = username
+    }
+    
+    init(postKey: String, dictionary: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let desc = dictionary["description"] as? String {
+            self._postDescription = desc
+        }
+        
+        if let imageUrl = dictionary["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = dictionary["likes"] as? Int {
+            self._likes = likes
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
