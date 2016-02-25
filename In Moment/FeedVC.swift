074a8345a383c575effe354f7a90e9ee20f501e4
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-25 20:11:09 +0900

    Add camera image picker and post request for makePost

--- In Moment/FeedVC.swift
+++ In Moment/FeedVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource,
     UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -77,7 +78,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource,
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         let post = posts[indexPath.row]
-        
+
         if post.imageUrl == nil {
             return 150
         } else {
@@ -87,13 +88,59 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource,
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         imagePicker.dismissViewControllerAnimated(true, completion: nil)
-        
+        imageSelectorPreview.image = image
     }
     
     @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func makePost(sender: AnyObject) {
+        if let txt = postField.text where txt != "" {
+            if let img = imageSelectorPreview.image {
+                let urlStr = "https://post.imageshack.us/upload_api.php"
+                let url = NSURL(string: urlStr)!
+                let imgData = UIImageJPEGRepresentation(img, 0.2)!
+                let keyData = "12DJKPSU5fc3afbd01b1630cc718cae3043220f3".dataUsingEncoding(NSUTF8StringEncoding)!
+                let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                Alamofire.upload(.POST, url, multipartFormData: { multipartFormData in
+                    
+                    multipartFormData.appendBodyPart(data: imgData, name: "fileupload", fileName: "image", mimeType: "image/jpg")
+                    multipartFormData.appendBodyPart(data: keyData, name: "key")
+                    multipartFormData.appendBodyPart(data: keyJSON, name: "format")
+                    
+                    }) { encodingResult in // happens when upload is done
+                        
+                        switch encodingResult {
+                        case .Success(let upload, _, _):
+                            //upload.responseJSON(completionHandler: { request, response, result in
+                            upload.responseJSON(completionHandler: { response in
+                                if let info = response.result.value as? Dictionary<String, AnyObject> {
+                                    
+                                    if let links = info["links"] as? Dictionary<String, AnyObject> {
+                                        if let imgLink = links["image_link"] as? String {
+                                            print("LINK: \(imgLink)")
+                                        }
+                                    }
+                                }
+
+                            })
+//                                if let info = result.value as? Dictionary<String, AnyObject> {
+//                                    
+//                                    if let links = info["links"] as? Dictionary<String, AnyObject> {
+//                                        if let imgLink = links["image_link"] as? String {
+//                                            print("LINK: \(imgLink)")
+//                                        }
+//                                    }
+//                                }
+//                            })
+                        case .Failure(let error):
+                            print(error)
+                        }
+                }
+            }
+        }
     }
 }
 
